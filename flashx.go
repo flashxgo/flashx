a238e5b8c3846a104ecd996dffa16dba19bcf519
Tak1za@Ubuntu <varungupta2015135@gmail.com>
2020-12-16 16:16:15 +0530

    Added default values for modify request and response

--- flashx.go
+++ flashx.go
@@ -15,6 +15,7 @@ type Engine struct {
 	// ModifyRequest allows you to modify the request before sending it
 	// It accepts a function that alters the request to be sent.
 	// Accepted function must not access the provided request after returning
+	// If not set, a default value will be picked up
 	ModifyRequest func(*http.Request)
 
 	// ModifyResponse allows you to modify the response once it is received
@@ -22,6 +23,7 @@ type Engine struct {
 	// If ModifyResponse returns an error, ErrorHandler is called
 	// with its error value. If ErrorHandler is nil, its default
 	// implementation is used.
+	// If not set, a default value will be picked up
 	ModifyResponse func(*http.Response) error
 }
 
@@ -30,14 +32,18 @@ var (
 )
 
 // Setup creates a reverse proxy for the configured URL
-func (e *Engine) Setup(url string, writer http.ResponseWriter, request *http.Request) error {
-	endpoint, err := parseURLs(url)
-	if err != nil {
-		return errMalformedURL
+func (e *Engine) Setup(url *url.URL, writer http.ResponseWriter, request *http.Request) error {
+	revProxy := httputil.NewSingleHostReverseProxy(url)
+	if e.ModifyRequest == nil {
+		e.ModifyRequest = defaultDirector(url)
 	}
-	revProxy := httputil.NewSingleHostReverseProxy(endpoint)
 	revProxy.Director = e.ModifyRequest
+
+	if e.ModifyResponse == nil {
+		e.ModifyResponse = defaultModifyResponse()
+	}
 	revProxy.ModifyResponse = e.ModifyResponse
+
 	revProxy.ServeHTTP(writer, request)
 	return nil
 }
@@ -48,4 +54,19 @@ func parseURLs(urls string) (*url.URL, error) {
 		return nil, err
 	}
 	return url, nil
+}
+
+func defaultDirector(url *url.URL) func(req *http.Request) {
+	return func(req *http.Request) {
+		req.URL.Host = url.Host
+		req.URL.Scheme = url.Scheme
+		req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+		req.Host = url.Host
+	}
+}
+
+func defaultModifyResponse() func(*http.Response) error {
+	return func(h *http.Response) error {
+		return nil
+	}
 }
